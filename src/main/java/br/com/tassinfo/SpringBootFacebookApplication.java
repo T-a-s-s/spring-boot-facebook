3211d558a9570dcf211a7f662e1aca9e203c25cd
T-a-s-s <tomas.liveid@hotmail.com>
2018-03-23 17:31:17 -0300

    inicio feito atravez do exemplo, precisa ser adicionado https

---
+++ src/main/java/br/com/tassinfo/SpringBootFacebookApplication.java
@@ -0,0 +1,12 @@
+package br.com.tassinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFacebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFacebookApplication.class, args);
+	}
+}
